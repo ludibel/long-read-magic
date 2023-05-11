4f2e764867b6f9f5889e9e9f0bba0faecf258fbf
Anne Staff <anne.staff@pm.me>
2023-05-11 23:05:52 +0200

    added manifest generation

---
+++ src/generate-manifest.ts
@@ -0,0 +1,151 @@
+import { S3 } from "@aws-sdk/client-s3";
+import * as fs from "fs";
+import { MetadataItem } from "./utils/models";
+
+// Configure the AWS SDK
+const s3 = new S3({
+    region: "eu-central-1",
+    credentials: {
+        accessKeyId: "AKIAXD2CIN2C7NLMJ3PT",
+        secretAccessKey: "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"
+    },
+});
+const bucketName = "gene-stag";
+
+
+// Define CSV field names
+const fieldNames = [
+    "filename", "completeness", "contamination", "completeness_model_used", "translation_table_used", "coding_density",
+    "contig_n50", "average_gene_length", "genome_size", "gc_content", "total_coding_sequences", "additional_notes",
+    "n_genes_called", "n_genes_mapped", "n_contigs", "taxonomic_level", "proportion_genes_retained_in_major_clades",
+    "genes_retained_index", "clade_separation_score", "contamination_portion", "n_effective_surplus_clades",
+    "mean_hit_identity", "reference_representation_score", "passGnuc", "trna", "16s", "5s", "23s"
+];
+
+// Define the fields to keep in the output JSON
+const fieldsToKeep = [
+    "filename", "completeness", "contamination", "passGnuc"
+];
+
+async function csvToJson(input: string): Promise<any> {
+    let lines = input.split("\n");
+    lines = lines.slice(1)
+
+    const resultArray: MetadataItem[] = [];
+
+
+    lines.forEach(line => {
+        const values = line.split(",");
+
+        // skip empty lines
+        if (values.length <= 1) {
+            return;
+        }
+
+        const item: MetadataItem = {};
+
+        fieldNames.forEach((fieldName, index) => {
+            const key = fieldNames[index];
+            const value = values[index];
+
+            if (!fieldsToKeep.includes(key)) {
+                return;
+            }
+
+            if (key === "filename") {
+                item[key] = `${value}.fa`;
+            } else if (!isNaN(parseFloat(value))) {
+                //@ts-ignore
+                item[key] = parseFloat(value);
+            } else if (key === "passGnuc") {
+                //@ts-ignore
+                item[key] = value.toLowerCase() === "true"
+            } else {
+                //@ts-ignore
+                item[key] = value;
+            }
+        });
+
+        resultArray.push(item);
+    })
+
+    return {"items": resultArray};
+
+}
+
+function appendDownloadLink(json: { items: MetadataItem[] }, linkPrefix: string): void {
+    json.items.forEach((item) => {
+        item.downloadLink = `${linkPrefix}/${item.filename}`;
+    });
+}
+
+async function processS3Bucket() {
+    try {
+        const projects: Record<string, any> = {};
+
+        // List all project folders in the root level of the bucket
+        const projectFolders = await s3.listObjects({Bucket: bucketName, Delimiter: "/"});
+
+        if (projectFolders.CommonPrefixes === undefined) {
+            throw new Error("No project folders found in the root level of the bucket.");
+        }
+
+        for (const projectFolder of projectFolders.CommonPrefixes) {
+            if (projectFolder.Prefix === undefined) {
+                continue;
+            }
+
+            const projectName = projectFolder.Prefix.split("/")[0];
+
+            // List all project variation folders in the current project folder
+            const projectVariationFolders = await s3
+                .listObjects({Bucket: bucketName, Delimiter: "/", Prefix: projectFolder.Prefix});
+
+            if (projectVariationFolders.CommonPrefixes === undefined) {
+                throw new Error(`No project variation folders found in the ${projectFolder.Prefix} folder.`);
+            }
+
+            for (const projectVariationFolder of projectVariationFolders.CommonPrefixes) {
+                if (projectVariationFolder.Prefix === undefined) {
+                    throw new Error(`No project variation folders found in the ${projectFolder.Prefix} folder.`);
+                }
+
+                const projectVariationName = projectVariationFolder.Prefix.split("/")[1];
+
+                // Download the result.csv file from the current project variation folder
+                const csvMetadataFileKey = `${projectVariationFolder.Prefix}result.csv`;
+                const csvMetadataFile = await s3.getObject({Bucket: bucketName, Key: csvMetadataFileKey});
+
+                if (csvMetadataFile.Body === undefined) {
+                    throw new Error(`No result.csv file found in ${csvMetadataFileKey}`)
+                }
+
+                const csvMetadata = await csvMetadataFile.Body.transformToString()
+
+                const jsonOutput = await csvToJson(csvMetadata);
+                const linkPrefix = `https://gene-stag.s3.eu-central-1.amazonaws.com/${projectName}/${projectVariationName}/output_bins`;
+
+                appendDownloadLink(jsonOutput, linkPrefix);
+
+
+                // Add the JSON output to the projects object
+                if (!projects[projectName]) {
+                    projects[projectName] = {};
+                }
+                projects[projectName][projectVariationName] = jsonOutput.items;
+            }
+        }
+
+        // Save the final JSON structure to a file
+        const json = {projects};
+        fs.writeFileSync("public/data-manifest.json", JSON.stringify(json, null, 2));
+        console.log("Final JSON output saved");
+    } catch (error) {
+        console.error("Error processing S3 bucket:", error);
+    }
+}
+
+// Run the processS3Bucket function
+processS3Bucket()
+    .then(() => console.log("S3 bucket processed successfully."))
+    .catch((error) => console.error("Error processing S3 bucket:", error));
---
+++ src/utils/models.ts
@@ -0,0 +1,7 @@
+export type MetadataItem = {
+    filename?: string,
+    completeness?: number,
+    cotamination?: number,
+    passGnuc?: boolean,
+    downloadLink?: string,
+}
