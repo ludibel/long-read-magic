e96b371e23e32796b37bc656c93bc52baec7a156
ludibel <ludibelle@outlook.com>
2023-08-16 13:23:58 +0000

    layout Thanks

---
+++ content/thankPage/hero.md
@@ -0,0 +1,6 @@
+---
+title: Thank You
+description: One of our team members will be in touch with you shortly.
+---
+
+ThankPage Hero content
---
+++ src/pages/thanks.tsx
@@ -0,0 +1,57 @@
+import Head from 'next/head'
+import React from 'react'
+import Image from 'next/image'
+
+import { AttributesProps } from '../utils/types'
+import { attributes } from '@/content/thankPage/hero.md'
+
+import imageContact from '@/public/images/about_contact-min.png'
+import Link from 'next/link'
+
+export const Thanks = () => {
+  const { title, description } = attributes as AttributesProps
+  return (
+    <>
+      <Head>
+        <title>Contact</title>
+        <meta name="description" content="Contact form" />
+      </Head>
+
+      <div className="relative h-[777px] pt-[160px] xl:pb-[427px] xl:pr-[646px]">
+        <Image
+          src={imageContact}
+          alt="Image stem cells"
+          fill
+          className="bg-lightgray bg-opacity-50 object-cover object-center"
+        />
+        <div
+          className="h-100% absolute inset-0 z-0"
+          style={{
+            background:
+              'linear-gradient(91deg, #001135 0%, rgba(0, 17, 53, 0.00) 100%)',
+          }}
+        ></div>
+        <div className="z-1 relative flex flex-col gap-6 px-4 pb-[120px] pt-[160px] md:px-[54px] 2xl:pl-[200px]">
+          <div className="flex">
+            <h1 className="font-inter text-4xl font-semibold capitalize text-white lg:text-[52px] lg:leading-[4rem] 2xl:text-7xl">
+              {title} !
+            </h1>
+          </div>
+          <div>
+            <p className="font-inter gap-x-6 text-sm font-normal leading-6 text-white sm:text-[21px] md:leading-9 2xl:text-2xl">
+              {description}
+            </p>
+            <p className="font-inter gap-x-6 text-sm font-normal leading-6 text-white sm:text-[21px] md:leading-9 2xl:text-2xl">
+              Before we get back to you, you can learn more{' '}
+              <Link href="/about" className="undeline-offset-1 underline">
+                about our project
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Thanks
